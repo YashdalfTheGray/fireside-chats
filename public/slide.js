c6806382b8588f71629335ca95aba7673ae7cec8
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-11-03 22:47:11 -0600

    added more to the slide controller and view

--- public/slide.js
+++ public/slide.js
@@ -3,11 +3,29 @@
 angular.module('firesideChats')
 .controller('SlideCtrl',
     [
-        '$rootScope',
-        function($rootScope) {
+        '$rootScope', '$state',
+        function($rootScope, $state) {
             "use strict";
 
             var vm = this;
+
+            vm.next = function() {
+                var slideNum = $state.current.name[$state.current.name.length - 1];
+                vm.openActions = false;
+                console.log('Navigating to next!');
+                console.log(slideNum);
+            };
+
+            vm.back = function() {
+                var slideNum = $state.current.name[$state.current.name.length - 1];
+                vm.openActions = false;
+                console.log('Navigating to previous!');
+            };
+
+            vm.home = function() {
+                vm.openActions = false;
+                $state.go('home');
+            }
         }
     ]
 );
